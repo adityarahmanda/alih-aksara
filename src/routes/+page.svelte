52d8fd48b4f0f98ac4148823e84df214b1224732
Aditya Rahmanda <arahmanda21@gmail.com>
2023-11-21 13:16:43 +0700

    update home page screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,5 +12,39 @@
 />
 
 <section>
-    Dalam tahap pengerjaan
+<div class="row is-vertical-align" style="margin-top: 4em; margin-bottom: 4em">
+  <div class="col">
+    <p>Alat transliterasi sederhana untuk mengalihkan/mengubah/mengkonversi teks aksara Jawa ke bentuk aksara lain ataupun sebaliknya.</p>
+    <a href="/latin-jawa" class="button primary">Coba Sekarang</a>
+  </div>
+  <div class="col is-right">
+    <picture>
+      <source type="image/webp" srcset="illustration.webp">
+      <img src="illustration.png"  alt="Illustration" width="420">
+    </picture>
+  </div>
+</div>
+<div class="row">
+  <div class="col-8">
+    <h4>Sekilas Tentang alih aksara</h4>
+    <p>Bisa dibilang proyek <strong>alih aksara</strong> ini hanyalah sebuah keisengan. Iseng karena ini memang bukan proyek serius apalagi resmi, sekadar proyek pribadi suka-suka yang saya manfaatkan untuk lebih dalam mempelajari aksara Jawa ataupun ilmu pemrograman web itu sendiri. Oleh karena itu, saya yang masih awam ini menerima segala umpan balik untuk mengembangkan website ini lebih baik.</p>
+  </div>
+  <div class="col">
+    <div style="margin-bottom: 2em">
+      <h4>Umpan Balik</h4>
+      <span>Hubungi saya lewat <a href="#" style="text-decoration: underline;">Email</a></span>
+    </div>
+    <div>
+      <h4>Mungkin kamu juga tertarik</h4>
+      <ul class="list-unstyled">
+        <li style="margin-bottom: .5em">
+          <a href="https://adityarahmanda.github.io/macaksara/" style="text-decoration: underline;">Macaksara - Website Permainan Kuis Aksara Jawa</a>
+        </li>
+        <li style="margin-bottom: .5em">
+          <a href="https://adityarahmanda.github.io/artikel/belajar-aksara-jawa" style="text-decoration: underline;">Belajar Aksara Jawa Lengkap Beserta Contohnya</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
 </section>
