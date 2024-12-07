c94adb8887cf93e06daf29dda321de940484d9db
Aditya Rahmanda <arahmanda21@gmail.com>
2024-12-07 13:41:50 +0700

    show email to feedback widget

--- src/lib/components/WidgetFeedback.svelte
+++ src/lib/components/WidgetFeedback.svelte
@@ -1,4 +1,4 @@
 <div {...$$props}>
     <h4>Umpan Balik</h4>
-    <span>Kirimkan saja melalui <a href="mailto: arahmanda21@gmail.com" style="text-decoration: underline;">Email</a> ini</span>
+    <span>Kirimkan saja melalui email <a href="mailto: arahmanda21@gmail.com" style="text-decoration: underline;">arahmanda21@gmail.com</a> ini</span>
 </div>
